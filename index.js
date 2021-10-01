535833774c60d27ad4dc0002bab620834169094c
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-01 07:59:35 +0300

    autj added to index but not working

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ connectDB();
 // Rout Files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+
 
 const app = express();
 
@@ -38,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('api/v1/auth', auth);
 // Error middleware
 app.use(errorHandler);
 
