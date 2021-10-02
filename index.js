602fb79ab60c1a0654359cf54444b6896a13286c
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-02 05:00:46 +0300

    problem with register route is fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const connectDB = require("./db");
 connectDB();
 
 
-// Route files
+// Rout Files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
@@ -36,7 +36,8 @@ app.use(fileUpload());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Mount routers
+//Mount routers
+
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
