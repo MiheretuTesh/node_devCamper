e6361af90337bca46666863c51092ff003a62b38
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-02 06:43:35 +0300

    Sign is working and generates jwtwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,9 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   })
 
-  res.status(200).json({ success: true });
+  // Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
  
--- models/User.js
+++ models/User.js
@@ -1,47 +1,47 @@
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const randomize = require('randomatic');
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const randomize = require("randomatic");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name'],
+    required: [true, "Please add a name"],
   },
   email: {
     type: String,
-    required: [true, 'Please add an email'],
+    required: [true, "Please add an email"],
     unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
+      "Please add a valid email",
     ],
   },
   role: {
     type: String,
-    enum: ['user', 'publisher'],
-    default: 'user',
+    enum: ["user", "publisher"],
+    default: "user",
   },
   password: {
     type: String,
-    required: [true, 'Please add a password'],
+    required: [true, "Please add a password"],
     minlength: 6,
     select: false,
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
-//   confirmEmailToken: String,
-//   isEmailConfirmed: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   twoFactorCode: String,
-//   twoFactorCodeExpire: Date,
-//   twoFactorEnable: {
-//     type: Boolean,
-//     default: false,
-//   },
+  //   confirmEmailToken: String,
+  //   isEmailConfirmed: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+  //   twoFactorCode: String,
+  //   twoFactorCodeExpire: Date,
+  //   twoFactorEnable: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
   createdAt: {
     type: Date,
     default: Date.now,
@@ -49,10 +49,15 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-UserSchema.pre('save', async function(next){
+UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.getSignedJwtToken = () =>
+  jwt.sign({ id: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
 
 // Encrypt password using bcrypt
 // UserSchema.pre('save', async function (next) {
@@ -108,4 +113,4 @@ UserSchema.pre('save', async function(next){
 //   return confirmTokenCombined;
 // };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
