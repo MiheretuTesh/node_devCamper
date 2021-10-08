cb3074b2546be7812734c9fd93f6829614764dd1
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-08 13:32:16 +0300

    hpp, request limit and cross origin resource sharing protection added

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ app.use(helmet());
 // Prevent XSS attacks
 app.use(xss());
 
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 // set static folder
 
 app.use(express.static(path.join(__dirname, 'public')));
